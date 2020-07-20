af462763325370fafd203475c8ae83667b569fff
achman1729 <achin.madot@gmail.com>
2020-07-20 14:32:23 +1000

    Added search bar

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Container, Row, Col} from "react-bootstrap"
 
 export default function Portfolio() {
     return (
-        <div>
-            Portfolio
-        </div>
+
+<Container>
+    <Row>
+        <Col>Card 1</Col>
+        <Col>Card 2</Col>
+    </Row>
+    <Row>
+        <Col>Card 3</Col>
+        <Col>Card 4</Col>
+    </Row>
+    <Row>
+        <Col>Card 5</Col>
+        <Col>Card 6</Col>
+    </Row>
+    <Row>
+        <Col>Card 7</Col>
+        <Col>Card 8</Col>
+    </Row>
+</Container>
     )
 }

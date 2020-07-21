cf82f5eefadebf41984876c997e733d4d007d05c
achman1729 <achin.madot@gmail.com>
2020-07-22 09:54:17 +1000

    made changes to componenets

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
-import {Container, Row, Col} from "react-bootstrap"
+// import {Container, Row, Col} from "react-bootstrap"
 
 export default function Portfolio() {
     return (
-
-<Container>
-    <Row>
-        <Col>Card 1</Col>
-        <Col>Card 2</Col>
-    </Row>
-    <Row>
-        <Col>Card 3</Col>
-        <Col>Card 4</Col>
-    </Row>
-    <Row>
-        <Col>Card 5</Col>
-        <Col>Card 6</Col>
-    </Row>
-    <Row>
-        <Col>Card 7</Col>
-        <Col>Card 8</Col>
-    </Row>
-</Container>
+        <div></div>
     )
 }

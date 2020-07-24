7cee28f84468093821b09141a703e7f5c7276776
achman1729 <achin.madot@gmail.com>
2020-07-24 20:15:33 +1000

    finished seting up the Auth functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,37 +1,89 @@
-import React from 'react'
-import { NavLink } from "react-router-dom"
+import React, { Component } from 'react'
+import { NavLink, withRouter } from "react-router-dom"
 import "../Stylesheets/Header.scss"
 
-export default function Header() {
-    return (
-        <div>
+class Header extends Component {
+
+    logOut(e) {
+        e.preventDefault()
+        localStorage.removeItem('usertoken')
+        this.props.history.push(`/`)
+      }
+
+    render(){
+        const loginReglink = (
             <nav className="header">
                 <NavLink
                 exact
-                activeClassName="navbar__link--active"
                 className="navbar__link"
-                to="/watchlist"
+                to="/login"
                 >
-                Watchlist
+                Login
                 </NavLink>
                 <NavLink
-                activeClassName="navbar__link--active"
+                exact
                 className="navbar__link"
-                to="/portfolio"
+                to="/register"
                 >
-                Portfolio
-                </NavLink>
-                <NavLink
-                activeClassName="navbar__link--active"
-                className="navbar__link"
-                to="/post"
-                >
-                Post
+                Register
                 </NavLink>
             </nav>
-            <form className="search-bar">
-                <input type="text" id="search" name="search" placeholder="Search Stock.." />
-            </form>
-        </div>
-    )
+        )
+
+        const userLink = (
+            <nav className="header">
+                <NavLink
+                exact
+                className="navbar__link"
+                to="/profile"
+                >
+                Profile
+                </NavLink>       
+                <NavLink
+                exact
+                className="navbar__link"
+                to=""
+                onClick={this.logOut.bind(this)}
+                >
+                Logout
+                </NavLink>
+            </nav>
+        )
+
+        return (
+            <div>
+                <nav className="header">
+                    <NavLink
+                    exact
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/watchlist"
+                    >
+                    Watchlist
+                    </NavLink>
+                    <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/portfolio"
+                    >
+                    Portfolio
+                    </NavLink>
+                    <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/post"
+                    >
+                    Post
+                    </NavLink>
+                    {localStorage.usertoken ? userLink : loginReglink}
+                </nav>
+                <form className="search-bar">
+                    <input type="text" id="search" name="search" placeholder="Search Stock.." />
+                </form>
+
+            </div>
+        )
+    }
 }
+// withRouter pass updated match, location, and history props to the wrapped component whenever it renders.
+export default withRouter(Header)
---
+++ src/Components/UserFunction.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+
+// the app is connected via the "proxy": "http://localhost:8000" in package.json
+
+export const register = newUser=> {
+    // posting to users/register the payload
+    return axios
+    .post('users/register', {
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      password: newUser.password
+    })
+    .then(response => {
+      console.log('Registered')
+    })
+}
+
+export const login = user => {
+    // we will be setting usertoken inside the local storage with the response.data
+    // response.data will contain the token which is the encoded object of our payload
+    return axios
+      .post('users/login', {
+        email: user.email,
+        password: user.password
+      })
+      .then(response => {
+        localStorage.setItem('usertoken', response.data)
+        return response.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+export const getProfile = user => {
+    return axios
+        .get('users/profile', {
+        //headers: { Authorization: ` ${this.getToken()}` }
+        })
+        .then(response => {
+        console.log(response)
+        return response.data
+        })
+        .catch(err => {
+        console.log(err)
+        })
+}

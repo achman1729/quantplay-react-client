3965353169f25614403ea50d8b1d4dfa5e847d9e
achman1729 <achin.madot@gmail.com>
2020-07-28 11:29:41 +1000

    render name and price of the stock

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 export default function Search() {
-    const defaultSymbol = "goog"
-    const token = "?token=cb0fa4bee45c5b25e4b7cdc1c74b3e54dd75720e"
+    const token = "token=cb0fa4bee45c5b25e4b7cdc1c74b3e54dd75720e"
     const tiingoApi = "/daily/"
-    const [symbol, setSymbol] = useState(defaultSymbol)
-    // const [bitcoinData, setBitcoinData] = useState({})
+    const [symbol, setSymbol] = useState()
+    const [price, setPrice] = useState()
+    const [name, setName] = useState()
+
+    // setting date to the latest date a price that can be retrieved
+    var today = new Date()
+    var dd = String(today.getDate() - 3).padStart(2, '0')
+    var mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+    var yyyy = today.getFullYear()
+    today = mm + '-' + dd + '-' + yyyy
+    const date = today
 
     const onChange = (e) => {
         setSymbol(e.target.value)
@@ -14,14 +22,20 @@ export default function Search() {
 
     const onSubmit = (e) => {
         e.preventDefault()
-
-        Axios.get(`${tiingoApi+symbol+token}`)
-        .then(response => {
-            console.log(response)
+        // constructing the API string
+        Axios.get(`${tiingoApi + symbol}/prices?startDate=${date}&format=json&&${token}`)
+            .then(response => {
+                console.log(response.data[0].close)
+                setPrice(response.data[0].close)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        Axios.get(`${tiingoApi + symbol}?${token}`)
+        .then(res => {
+            console.log(res.data.name)
+            // setName()
         })
-        .catch(error => {
-            console.log(error);
-          });
     }
 
     return (
@@ -30,6 +44,9 @@ export default function Search() {
                 <input type="text" id="search" name="search" placeholder="Search Stock.." onChange={onChange} />
                 <button>Search</button>
             </form>
+            <div>
+                <h1>Name: {name}</h1><h3>Price: {price}</h3>
+            </div>
         </div>
     )
 }

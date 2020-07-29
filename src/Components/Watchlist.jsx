6b271d2afa98426f6ab384e0b5fdc0d43cb72bad
achman1729 <achin.madot@gmail.com>
2020-07-29 20:23:17 +1000

    removed api call from the cards component

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CardStock from './Card'
+import ApiGet from '../ApiGet'
 
 export default function Watchlist() {
+    const { series, seriesVolume, companyName, ticker, description } = ApiGet('aapl', '2019-01-01', '2019-12-31')
     return (
         <div>
-            <CardStock />
-            <CardStock />
-            <CardStock />
+            <CardStock series={series} seriesVolume={seriesVolume} companyName={companyName} ticker={ticker} description={description} />
+            <CardStock series={series} seriesVolume={seriesVolume} companyName={companyName} ticker={ticker} description={description} />
+            <CardStock series={series} seriesVolume={seriesVolume} companyName={companyName} ticker={ticker} description={description} />
         </div>
     )
 }

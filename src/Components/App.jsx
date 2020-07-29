1cec4706a2c5abfab0b11a219d716ce0099d7ab7
achman1729 <achin.madot@gmail.com>
2020-07-29 14:21:31 +1000

    finished basic styling of search bar and rendered searchBar in App.js

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/watchlist" component={Watchlist} />
         <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/post" component={Post} />
+        {/* <Route exact path="/post" component={Post} /> */}
         <Route exact path="/search" component={SearchBar} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
